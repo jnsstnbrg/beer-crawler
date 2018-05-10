3e2b18b041dad773c2d2387d570b1a740bc9e77c
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-05-10 17:53:17 +0200

    Add comments

--- main.go
+++ main.go
@@ -19,6 +19,10 @@ type price struct {
 	Currency string  `json:"currency"`
 }
 
+type customTime struct {
+	time.Time
+}
+
 type product struct {
 	Number             string     `json:"nr"`
 	ArticleID          int32      `json:"article_id"`
@@ -46,12 +50,17 @@ func main() {
 
 // Handler is a lambda handler function
 func Handler() (string, error) {
+	// Save today's and a week ahead's date in variables
 	today := time.Now().Format("2006-01-02")
 	futureWeek := time.Now().AddDate(0, 0, 7).Format("2006-01-02")
+
+	// URL to where the json data exists
 	url := "https://bolaget.io/v1/products?sales_start_from=" + today + "&sales_start_to=" + futureWeek + "&limit=100&product_group=Öl&assortment=TSE"
+
+	// URL to systembolaget, is later sent to Slack
 	systembolagetURL := "https://www.systembolaget.se/sok-dryck/?assortmenttext=Sm%C3%A5%20partier&sellstartdatefrom=" + today + "&sellstartdateto=" + futureWeek + "&subcategory=%C3%96l&fullassortment=1"
 
-	// Create the request object
+	// Create request
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Println(err)
@@ -89,11 +98,16 @@ func sendToSlack(products []product, systembolagetURL string) error {
 	// Create buffer
 	var buffer bytes.Buffer
 
+	// Write topic(s) and url to buffer
 	buffer.WriteString(":beers: *Nytt ölsläpp inom en vecka!* :beers: (" + products[0].SalesStart.Format("2006-01-02") + ")\n")
 	buffer.WriteString(systembolagetURL + "\n\n")
 	buffer.WriteString("*Öl, Bryggeri, Pris, Storlek, ABV, Typ, Förpackning*\n")
+
 	for _, product := range products {
+		// Create product buffer
 		var productBuffer bytes.Buffer
+
+		// Write product specific information to buffer
 		productBuffer.WriteString("*" + product.Name + " " + product.AdditionalName + "*, ")
 		productBuffer.WriteString(product.Producer + ", ")
 		productBuffer.WriteString(strconv.FormatFloat(product.Price.Amount, 'f', 1, 64) + " " + product.Price.Currency + ", ")
@@ -102,19 +116,24 @@ func sendToSlack(products []product, systembolagetURL string) error {
 		productBuffer.WriteString(product.Style + ", ")
 		productBuffer.WriteString(product.Packing + ", ")
 		productBuffer.WriteString("\n")
+
+		// Write product buffer to main buffer
 		buffer.WriteString(productBuffer.String())
 	}
 
+	// Marshal buffer to json
 	data, err := json.Marshal(slackRequest{buffer.String()})
 	if err != nil {
 		return err
 	}
 
+	// Create request
 	req, err := http.NewRequest("POST", os.Getenv("SLACK_URL"), bytes.NewBuffer(data))
 	if err != nil {
 		return err
 	}
 
+	// Send request to Slack
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -125,12 +144,11 @@ func sendToSlack(products []product, systembolagetURL string) error {
 	return nil
 }
 
-type customTime struct {
-	time.Time
-}
-
 func (ct *customTime) UnmarshalJSON(b []byte) (err error) {
+	// Trim away "
 	s := strings.Trim(string(b), "\"")
+
+	// Parse time
 	ct.Time, err = time.Parse("2006-01-02", s)
 	return
 }
