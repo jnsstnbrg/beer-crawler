ff501eb8a4a01677fb5c54b9e9940945e7249287
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-06-01 13:51:14 +0200

    Use Systembolaget API

--- main.go
+++ main.go
@@ -3,17 +3,35 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
 	"time"
-
-	b "github.com/larsha/bolaget.io-sdk-go"
 )
 
 type slackRequest struct {
 	Text string `json:"text"`
+}
+
+type beverage struct {
+	ProductID         string  `json:"ProductId"`
+	SubCategory       string  `json:"SubCategory"`
+	ProductNameBold   string  `json:"ProductNameBold"`
+	ProductNameThin   string  `json:"ProductNameThin"`
+	ProducerName      string  `json:"ProducerName"`
+	Price             float64 `json:"Price"`
+	Volume            float64 `json:"Volume"`
+	AlcoholPercentage float64 `json:"AlcoholPercentage"`
+	Style             string  `json:"Style"`
+	BottleTextShort   string  `json:"BottleTextShort"`
+	SellStartDate     string  `json:"SellStartDate"`
+}
+
+type doc struct {
+	Beverages []beverage `json:"Hits"`
 }
 
 func main() {
@@ -21,54 +39,87 @@ func main() {
 	today := time.Now().Format("2006-01-02")
 	futureWeek := time.Now().AddDate(0, 0, 7).Format("2006-01-02")
 
-	// URL to where the json data exists
-	products, err := b.GetProducts(b.ProductQueryParams{
-		Assortment:     "TSE|TSLS|TSV|TST",
-		Limit:          100,
-		ProductGroup:   "Öl",
-		SalesStartFrom: today,
-		SalesStartTo:   futureWeek,
-	})
+	URL, err := url.Parse("https://api-extern.systembolaget.se/product/v1/product/search")
 	if err != nil {
-		log.Println(err)
-    return
+		panic("boom")
+	}
+
+	parameters := url.Values{}
+	parameters.Add("AssortmentText", "Lokalt & Småskaligt")
+	parameters.Add("SellStartDateFrom", today)
+	parameters.Add("SellStartDateTo", futureWeek)
+	parameters.Add("SubCategory", "Öl")
+	URL.RawQuery = parameters.Encode()
+
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	req, err := http.NewRequest("GET", URL.String(), nil)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.Header.Add("Ocp-Apim-Subscription-Key", os.Getenv("SUBSCRIPTION_KEY"))
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, readErr := ioutil.ReadAll(resp.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	doc := doc{}
+
+	jsonErr := json.Unmarshal(body, &doc)
+	if jsonErr != nil {
+		log.Fatalln(jsonErr)
 	}
 
 	// Check if products slice contains any items
-	if len(products) > 0 {
+	if len(doc.Beverages) > 0 {
 		// URL to systembolaget, is later sent to Slack
 		systembolagetURL := "https://www.systembolaget.se/sok-dryck/?assortmenttext=Sm%C3%A5%20partier&sellstartdatefrom=" + today + "&sellstartdateto=" + futureWeek + "&subcategory=%C3%96l&fullassortment=1"
 
 		// Send to Slack
-		err = sendToSlack(products, systembolagetURL)
+		err = sendToSlack(doc.Beverages, systembolagetURL)
 		if err != nil {
 			log.Println(err.Error())
-      return
+			return
 		}
 	}
 }
 
-func sendToSlack(products []b.Product, systembolagetURL string) error {
+func sendToSlack(beverages []beverage, systembolagetURL string) error {
 	// Create buffer
 	var buffer bytes.Buffer
 
 	// Write topic(s) and url to buffer
-	buffer.WriteString(":beers: *Nytt ölsläpp inom en vecka!* :beers: (" + products[0].SalesStart + ")\n")
+	buffer.WriteString(":beers: *Nytt ölsläpp inom en vecka!* :beers: (" + beverages[0].SellStartDate + ")\n")
 	buffer.WriteString(systembolagetURL + "\n\n")
 	buffer.WriteString("*Öl, Bryggeri, Pris, Storlek, ABV, Typ, Förpackning*\n")
 
-	for _, product := range products {
+	for _, product := range beverages {
 		// Create product buffer
 		var productBuffer bytes.Buffer
 
 		// Write product specific information to buffer
-		productBuffer.WriteString("*" + product.Name + " " + product.AdditionalName + "*, ")
-		productBuffer.WriteString(product.Producer + ", ")
-		productBuffer.WriteString(strconv.FormatFloat(product.Price.Amount, 'f', 2, 64) + " " + product.Price.Currency + ", ")
-		productBuffer.WriteString(strconv.FormatFloat(product.VolumeInMilliliter, 'f', 0, 64) + " ml, ")
-		productBuffer.WriteString(product.Alcohol + ", ")
+		productBuffer.WriteString("*" + product.ProductNameBold + "*")
+		if product.ProductNameThin != "" {
+			productBuffer.WriteString(" *" + product.ProductNameThin + "*")
+		}
+		productBuffer.WriteString(", ")
+		productBuffer.WriteString(product.ProducerName + ", ")
+		productBuffer.WriteString(strconv.FormatFloat(product.Price, 'f', 2, 64) + " SEK, ")
+		productBuffer.WriteString(strconv.FormatFloat(product.Volume, 'f', 0, 64) + " ml, ")
+		productBuffer.WriteString(strconv.FormatFloat(product.AlcoholPercentage, 'f', 0, 64) + "%, ")
 		productBuffer.WriteString(product.Style + ", ")
-		productBuffer.WriteString(product.Packaging + ", ")
+		productBuffer.WriteString(product.BottleTextShort + ", ")
 		productBuffer.WriteString("\n")
 
 		// Write product buffer to main buffer
@@ -93,7 +144,7 @@ func sendToSlack(products []b.Product, systembolagetURL string) error {
 	if err != nil {
 		return err
 	}
-  defer resp.Body.Close()
+	defer resp.Body.Close()
 
 	return nil
 }
